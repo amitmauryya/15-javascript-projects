cdb277a756bc0c5ccb96a980a459c0eb3ee0e517
amitmauryya <mauryaamit2062@gmail.com>
2025-07-31 18:19:38 +0530

    Add files via upload

---
+++ password_generator/script.js
@@ -0,0 +1,23 @@
+gsap.from("h2", {
+    y: -800,
+    duration: 3,
+    opacity: 0,
+})
+gsap.from("button", {
+    y: -800,
+    duration: 3,
+    opacity: 0,
+    // stagger: true,
+})
+gsap.from("input", {
+    y: -800,
+    duration: 3,
+    opacity: 0,
+    // stagger: true,
+})
+gsap.from("img", {
+    y: -800,
+    duration: 3,
+    opacity: 0,
+    // stagger: true,
+})
